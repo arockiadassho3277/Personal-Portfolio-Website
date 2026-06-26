02a385db672fadb173a72d611d9e310db9fe6849
arockiadassho3277 <arockiadassho@gmail.com>
2026-06-26 09:37:57 +0530

    Personal Portfolio Website

---
+++ javascript.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you for your message! I’ll get back to you soon.");
+});
